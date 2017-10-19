718a5d4913e74be88eb2ebf5b9bcc912d86ce245
WallysonNunes <wallyson.n.a.lima@gmail.com>
2017-10-19 20:16:06 -0200

    first commit

---
+++ app/src/main/assets/js/drawGraph.js
@@ -0,0 +1,36 @@
+function showPieChart(dataset, height, width)
+{
+    alert("Entrou Aqui !!\n");
+    var widthScale = d3.scale.linear()
+                  .domain([0, 90])
+                  .range([0, width]);
+
+    var color = d3.scale.linear()
+                  .domain([0, 90])
+                  .range(["red", "blue"]);
+
+
+    var axis = d3.svg.axis()
+        .ticks(5)
+        .scale(widthScale);
+
+    var canvas = d3.select("p").append("svg")
+                   .attr("width", 500)
+                   .attr("height", 500)
+                   .append("g")
+                   .attr("transform", "translate(20, 20)");
+
+
+    var bars = canvas.selectAll("rect")
+                  .data(dataArray)
+                  .enter()
+                    .append("rect")
+                    .attr("width", function(d) { return widthScale(d); })
+                    .attr("height", 50)
+                    .attr("fill", function(d) { return color(d); })
+                    .attr("y", function(d, i) { return i * 60 } );
+
+    canvas.append("g")
+      .attr("transform", "translate(0, 400)")
+      .call(axis);
+}
---
+++ app/src/main/java/wallyson/lima/mobivitool/view/MainActivity.java
@@ -0,0 +1,28 @@
+package wallyson.lima.mobivitool.view;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import wallyson.lima.mobivitool.R;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageButton btTimeline;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btTimeline = (ImageButton) findViewById(R.id.btTimeline);
+
+        btTimeline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TimelineActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}

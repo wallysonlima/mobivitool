75eb4bb0c5afcf493e5f6632b79f143e2affd887
WallysonNunes <wallyson.n.a.lima@gmail.com>
2017-11-26 10:59:26 -0200

    Created MultiActivity

---
+++ app/src/main/java/wallyson/lima/mobivitool/view/MultiLineActivity.java
@@ -0,0 +1,108 @@
+package wallyson.lima.mobivitool.view;
+
+import android.content.res.AssetManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import wallyson.lima.mobivitool.R;
+import wallyson.lima.mobivitool.presenter.MultiInterface;
+import wallyson.lima.mobivitool.presenter.MultiPresenter;
+import wallyson.lima.mobivitool.presenter.TimelinePresenter;
+
+public class MultiLineActivity extends AppCompatActivity implements MultiInterface {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private WebView webview;
+    private MultiPresenter mPresenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_line);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.multiLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        webview = (WebView) findViewById(R.id.webviewmulti);
+        mPresenter = new MultiPresenter(this, this.getApplicationContext(), webview);
+
+        //load the chart
+        loadChart("html/timelineChart.html");
+    }
+
+    // initialize the WebView and the pie chart
+    public void loadChart(String remoteUrl)
+    {
+        String html = "";
+        webview.setWebViewClient(new WebViewClient());
+        WebSettings webSettings =
+                webview.getSettings();
+
+        webview.addJavascriptInterface(mPresenter, "AndroidTimeline");
+        webview.setWebContentsDebuggingEnabled(true);
+
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setDefaultTextEncodingName("utf-8");
+
+
+        AssetManager mgr = getBaseContext().getAssets();
+        try {
+            InputStream in = mgr.open(remoteUrl, AssetManager.ACCESS_BUFFER);
+            html = readHtml(in);
+            in.close();
+            webview.loadDataWithBaseURL("file:///android_asset/", html, "text/html", "UTF-8", null);
+            //webview.loadUrl("file:///android_asset/html/piechart.html");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String readHtml(InputStream in) throws IOException {
+        if(in == null) {
+            return "";
+        }
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            int n;
+            while ((n = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0, n);
+            }
+        } finally {
+        }
+        return writer.toString();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

63a5e4a5686541e55d489bddaa6fbfd3650633fb
WallysonNunes <wallyson.n.a.lima@gmail.com>
2018-01-08 16:08:10 -0200

    Created class _Default

---
+++ app/src/main/java/wallyson/lima/mobivitool/dao/_Default.java
@@ -0,0 +1,31 @@
+package wallyson.lima.mobivitool.dao;
+
+/**
+ * Created by wlima on 1/8/18.
+ */
+
+public class _Default {
+    protected String _mensagem;
+    protected boolean _status;
+
+    public _Default() {
+        this._mensagem = "";
+        this._status = true;
+    }
+
+    public boolean is_status() {
+        return _status;
+    }
+
+    public void set_status(boolean _status) {
+        this._status = _status;
+    }
+
+    public String get_mensagem() {
+        return _mensagem;
+    }
+
+    public void set_mensagem(String _mensagem) {
+        this._mensagem = _mensagem;
+    }
+}
